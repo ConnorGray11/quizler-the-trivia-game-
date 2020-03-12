c5401c48fc795859ca220541fe0f21d6d8111671
Connor T Gray <cgray@debian07.CIS.COM>
2020-03-12 09:34:14 -0500

    ss

--- Games_make.py
+++ Games_make.py
@@ -2,10 +2,16 @@ import pickle
 
 Questions = {1:["What is the main character of Halo?", "Private Pile", "Master Chief", "Frost", "Soap", 2],
              2:["Which pokemon has no type disadvantage?", "Dugtrio", "Shuckle", "Tepig", "Eelectros", 4], 
-             3:["What are the proffesions of Mario and his brother Luigi?", "Teachers", "Exterminators", "Plumbers", "Zookeepers", 3],
+             3:["What are the professions of Mario and his brother Luigi?", "Teachers", "Exterminators", "Plumbers", "Zookeepers", 3],
              4:["In which of these countries did the Nintendo Wii NOT come bundled with a copy of Wii Sports?", "South Korea", "Russia", "Canada", "Japan", 4],
-             5:["Question 5", "reee1", "reee2", "reee3", "reee4", 1],
-             6:["Question 6", "reeee1", "reeee2", "reeee3", "reeee4", 4] }
+             5:["What Board game helped POW's escape during WW2", "Monopoly","Chutes and Latters",  "Battleship", "Candy land", 1],
+             6:["What country is tetris from", "North Korea", "Kenya", "Ireland", "Russia", 4], 
+             7:["In which game can you find dog meat in cake of broken dreams", "Star wars:KOTOR","Fallout 2",  "Final Fantasy", "EarthBound", 2],
+             8:["What does Atari mean", "Winner","Best",  "Success", "Revolution", 3], 
+             9:["When was minecraft released", "2009","1998",  "2006", "2016", 1],
+             10:["Who was the enemy in Homefront", "The U.S Govt","The Korean army",  "Space Nazis", "Aliens", 2],
+             11:["What game is the character master hands portayed in", "Halo","Call of duty",  "Street Fighter", "Smash", 4],
+             12:["In the metal gear series who is Liquid snakes brother", "Snakeus Wakeus","Ho Chi Minh",  "Solid Snake", "Zoidberg", 3],}
 
 games = open("games.pickle","wb")
 pickle.dump(Questions, games)
--- Music_make.py
+++ Music_make.py
@@ -1,9 +1,18 @@
 import pickle
 
-games = {1:['FPS','Halo 3','Bungie','Microsoft','Xbox','2007','10','either','30.00','yes','1/15/2008','good game yee'],
-2:['RTS','Halo Wars','Bungie','Microsoft','Xbox','2009','10','either','20.00','yes','6/20/2009','loved the art style']}
+Questions = {1:["What is the highest note on a baseline piano?", "B7", "C8", "G2", "A1", 2],
+             2:["When was Daft Punks first album released?", "2000", "1999", "1995", "1997", 4], 
+             3:["What Depeche Mode song was inspired by Priscilla Presley's book Elvis and Me?", "Strangelove", "World in my eyes", "Personal Jesus", "sweetest Perfection", 3],
+             4:["What instrument do you play without touching it?", "Sitar", "Harp", "synthesizer", "theremin", 4],
+             5:["How much pressure do all the strings combined within a grand piano exert on the frame?", "30 tons","300 tons",  "3 tons", "300 lbs", 1],
+             6:["What is the oldest surviving musical instrument?", "Drum", "didgeridoo", "Lyre", "Flute", 4], 
+             7:["Which jazz musician was known for playing a bent trumpet?", "Chet Baker","Dizzy Gillespie",  "Miles Davis", "Louis Armstrong0", 2],
+             8:["What note is the lowest string on a bass guitar most commonly tuned to?", "G3","F4",  "E2", "B2", 3], 
+             9:[" How many different instruments did Prince play on his debut album?", "27","17",  "31", "26", 1],
+             10:["What 1985 charity single sold more than 20 million copies?", "Candle in the wind","we are the world",  "Thats what friends are for", "Do they know its christmas", 2],
+             11:["Who was The Number one Jazz Musican", "Billie Holiday","Nat King Cole",  "Lena Horne", "Louis Armstrong", 4],
+             12:["How many tonnes of bells are in the world's largest Carillon?", "123","78",  "91", "212", 3],}
 
-data_file = open("game_lib.pickle","wb")
-pickle.dump(games, data_file)
-data_file.close
-
+music = open("music.pickle","wb")
+pickle.dump(Questions, music)
+music.close
